73919552c13a898adad1e8b1f7d99fb48574ed48
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-11-08 23:41:46 +0530

    grading complete

--- Assignment-1/grading-students.cpp
+++ Assignment-1/grading-students.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 int main(){
     int n;
-    int arr[n];
     cout<<"enter number of students: ";
     cin>>n;
+    int grades[n];
     for (int i=0;i<n;i++){
-        cout<<"enter grade: ";
-        cin>>arr[i];
+        //cout<<"enter grade: ";
+        cin>>grades[i];
+    }
+    int div,rnd,diff;
+    int finalgrades[n];
+    for(int i=0;i<n;i++){
+        div=grades[i]/5;
+        div++;
+        rnd=5*div;
+        diff=rnd-grades[i];
+        //cout<<div<<" "<<diff<<" "<<rnd<<endl;
+        if(diff<3){
+            finalgrades[i]=rnd;
+        }
+        else if(diff>=3 || grades[i]<38){
+            finalgrades[i]=grades[i];
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<finalgrades[i]<<endl;
     }
     return 0;
 }
