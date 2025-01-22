a6d169f09b742cfe5901219069b7d78fb734db8e
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-01-22 16:14:15 +0530

    new-year-chaos

---
+++ Assignment-2/common-child.cpp
@@ -0,0 +1,12 @@
+//NOT DONE
+#include <iostream>
+#include <string>
+using namespace std;
+int commonChild(str s1, str s2){
+    return 0;
+}
+
+int main(){
+    return 0;
+
+}
---
+++ Assignment-2/new-year-chaos.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int minimumBribes(int n,int arr[n]){
+    int Bcount=0;
+    int temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j+1]<arr[j]){
+                temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+                Bcount+=1;
+            }
+        }
+    }
+    return Bcount;
+}
+int main(){
+    int t;
+    cin>>t;
+    int ans[t];
+    int n;
+    for(int i=0;i<t;i++){
+        cin>>n;
+        int arr[n];
+        int tempnum;
+        for(int j=0;j<n;j++){
+            cin>>arr[j];
+        }
+        tempnum=minimumBribes(n,arr);
+        ans[i]=tempnum;
+    }
+    cout<<endl;
+    for(int i=0;i<t;i++){
+        int num=ans[i];
+        if(num<=2){
+            cout<<num<<endl;
+        }
+        else{
+            cout<<"Too chaotic"<<endl;
+        }
+    }
+}
